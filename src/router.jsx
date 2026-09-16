7a4d2acec0d6f8b3ea29f1a8df8628f59b3005bb
Andreeagaldea <andreeagaldea@gmail.com>
2026-09-16 19:04:42 +0300

    fix scroll bug

--- src/router.jsx
+++ src/router.jsx
@@ -1,15 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
+import Layout from './components/Layout';
 
 export const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/projects',
-    element: <Projects />,
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/projects',
+        element: <Projects />,
+      },
+    ],
   },
 ], {
   basename: '/cv',
